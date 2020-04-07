a1aadbbe28887850cbf8b4c2c2dcab92bff40e3a
Fabian Schmidt <fabis@outlook.com>
2020-04-07 22:24:14 +0200

    minor fix in retrieving all games

--- games_db.py
+++ games_db.py
@@ -18,10 +18,11 @@ class GamesDB():
     def update_games(self, games):
         for game in games:  
             print(game['GAME_ID'])
+
             result  = GamesDB.games_db.update(
                 {
                     "GAME_ID" : game["GAME_ID"],
-                    "TEAM_CITY_NAME" : game["TEAM_CITY_NAME"]
+                    "TEAM_ID" : game["TEAM_ID"]
                 }, # Query parameter
                 game, # Replacement document
                 True # Upsert option --> Insert if query not matched, otherwise update
@@ -37,7 +38,7 @@ class GamesDB():
         for game in GamesDB.games_db.find():
             #print(game)
             games.append(game)
-        return games
+        return GamesDB.games_db.find()
 
     def remove_all(self):
         GamesDB.games_db.remove()
